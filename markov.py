be7eb9d06bd57263f80dcb2565f62ba077c29c14
Hua Szu Yang <33264733+huaszu@users.noreply.github.com>
2022-10-04 19:07:32 -0400

    Work in Progress
    Co-authored-by: andreamartz 14165788+andreamartz@users.noreply.github.com
    Co-authored-by: huaszu 33264733+huaszu@users.noreply.github.com

--- markov.py
+++ markov.py
@@ -23,11 +23,12 @@ def open_and_read_file(file_path):
         # print(line)
         words = words + line + " "
         # print(words)
-
+    words = words[0:-1:1]
+    
     return words
 
 
-open_and_read_file('green-eggs.txt')
+# open_and_read_file('green-eggs.txt')
 
 def make_chains(text_string):
     """Take input text as string; return dictionary of Markov chains.
@@ -54,32 +55,117 @@ def make_chains(text_string):
         [None]
     """
 
+    # take input string (representing the file contents) and turn it into an
+        # list of words) # ["Would", "you", "could", "you",...]
+    # iterate through the words 
+        # and get a slice of three words
+
+        # add an entry to chains where the key is the tuple containing the first
+            # two words, and the value is an array containing whatever
+            # it contained before PLUS the third word in the slice
+        # we'll do this by checking if the key is already in chains
+            # if it is, just add the third word in the slice to the list value
+            # if it is not, 
+    
     chains = {}
+    words_list = text_string.split(" ")
+    # print("WORDS_LIST: ", words_list)
+
+    end_index = len(words_list) - 2
+
+    for idx, word in enumerate(words_list):
+        if idx == end_index:
+            break
+        words_slice = words_list[idx:idx+3]
+        bigram = (words_slice[0], words_slice[1])
+        # feedback: bigram = (words_list[idx], words_list[idx+1])
+        print(chains.get(bigram, False))
+        if chains.get(bigram, False):
+            chains[bigram].append(words_slice[2])
+        else:
+            chains[bigram] = [words_slice[2]]
+        # print(bigram in chains)
+    
+    print(chains)   
+        # print(bigram)
+        # print("TEST:", words_slice)
+
+        # print("ENUMERATE:", idx, word)
+    
+
+    # chains = {
+    #     ('Would', 'you'): ['could', 'could', 'could', 'could', 'like'],
+    #     ('you,', 'could'): ['you', 'you', 'you', 'you'],
+    #     ('could', 'you'):  ['in', 'with', 'in', 'with'],
+    #     ('you', 'in'):     ['a', 'a'],
+    #     ('in', 'a'):       ['house?', 'box?'],
+    #     # ...
+    #     ('Sam', 'I'):      ['am?']
+    # }
+
+
+
 
     # your code goes here
 
     return chains
 
+text = open_and_read_file('green-eggs.txt')
+make_chains(text)
+# make_chains(text)
 
 def make_text(chains):
     """Return text from chains."""
 
+    from random import choice
+    
+    # words = ['cat', 'hat', 'sprinkler']
     words = []
+    # print("CHAINS: ", chains)
+    # print("TYPE: ", type(chains.keys()), chains.keys())
 
-    # your code goes here
+    # pick a starting point
+        # randomly pick a key from chains
+        # randomly pick a next word from the value associated with that key
+        # together, these three words are our link
+    # add link to our words list
+    # use the rightmost two words in the words list to figure out what our next
+    # key is.
+    # using that key, randomly pick a next word from the value associated
+    # add this word to the words list.
+    # we keep doing this until we are looking for a key that is not in chains.
+    # make a string out of the words list
 
+    start = choice(list(chains.keys()))  # tuple
+    #iterate
+    next_word = choice(chains[start])   # string
+    words.append(start[0])
+    words.append(start[1])
+    words.append(next_word)
+
+    print("START: ", type(start), "NEXT_WORD: ", type(next_word))
+
+    
     return ' '.join(words)
 
 
-# input_path = 'green-eggs.txt'
 
-# # Open the file and turn it into one long string
-# input_text = open_and_read_file(input_path)
+input_path = 'green-eggs.txt'
+# input_path = 'gettysburg.txt'
 
-# # Get a Markov chain
-# chains = make_chains(input_text)
+# Open the file and turn it into one long string
+input_text = open_and_read_file(input_path)
 
-# # Produce random text
-# random_text = make_text(chains)
 
-# print(random_text)
+# Get a Markov chain
+chains = make_chains(input_text)
+
+# Produce random text
+random_text = make_text(chains)
+
+print("RANDOM TEXT: ", random_text)
+
+
+
+# print(input_text)
+# print(range(len(input_text)))
